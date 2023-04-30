37eea854bb0a3e9241c27479bf35bfc18d8c1fd5
kvo.52@mail.ru <kvo.52@mail.ru>
2023-04-30 18:16:08 +0300

    class AppNotes new metods

--- AppNotes/AppNotes.py
+++ AppNotes/AppNotes.py
@@ -14,5 +14,45 @@ class AppNotes:
         note.id =len(self.notes)+1
         self.notes.append(note)
         print ("Заметка добавлена")
-        
-                
+
+    def save_notes(self):
+        with open ("notes.csv", "w", newline='', encoding='utf-8') as f:
+            writer = csv.writer(f, delimiter=';')
+            writer.writerow(["id","title", "content", "created_at"])
+            for note in self.notes:
+                writer.writerow([note.id, note.title, note.content, note.created_at.isoformat()])
+        print ("Заметка сохранена")
+
+    def load_notes(self):
+        try:
+            with open("notes.csv", "r", encoding='utf-8') as f:
+                reader = csv.DictReader(f, delimiter=';')
+                self.notes = [Note.from_dict(row) for row in reader]
+            print("Заметки загружена")
+        except FileNotFoundError:
+            print("Сохраненных заметок не найдено")
+
+    def edit_note(self, id, title, content):
+        for note in self.notes:
+            if note.id == id:
+                note.title = title
+                note.content = content
+                print("Заметка отредактирована")
+                return
+        print("Заметка не найдена")
+
+    def delete_note(self, id):
+        for note in self.notes:
+            if note.id == id:
+                self.notes.remove(note)
+                print("Заметка удалена")
+                return
+        print("Заметка не найдена")
+
+    def search_notes(self, start_date, end_date):
+        filtered_notes = [note for note in self.notes if start_date <= note.created_at.date() <= end_date]
+        if filtered_notes:
+            for note in filtered_notes:
+                print(f"{note.id}: {note.title}\n{note.content}\nСоздано: {note.created_at}\n")
+        else:
+            print("Заметки не найдены")                       
