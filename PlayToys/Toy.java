691da73d9528fe3f02ad90e16d3e0d82699bfcef
kvo.52@mail.ru <kvo.52@mail.ru>
2023-05-01 09:39:22 +0300

    clscc & properties Toys

---
+++ PlayToys/Toy.java
@@ -0,0 +1,13 @@
+class Toy {
+    private int id;
+    private String name;
+    private int quantity;
+    private int probability;
+
+    public Toy (int id, String name, int quantity, int probability){
+        this.id=id;
+        this.name=name;
+        this.quantity=quantity;
+        this.probability=probability;
+    }
+}
